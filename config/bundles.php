120159d8d56017866c7691c3cdf25e299262be62
Maxiloud <maxime@les-tilleuls.coop>
2023-07-07 10:56:00 +0200

    Initialized EasyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
