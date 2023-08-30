343157b5415b6a0c2ad325c20f4d2145354170e8
Maxiloud <maxime@les-tilleuls.coop>
2023-08-30 14:24:38 +0200

    Add referer route

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Service\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+
 
 class CartController extends AbstractController
 {
@@ -23,19 +26,31 @@ class CartController extends AbstractController
 
     #[Route('/cart/add/{id}', 'app_cart_add')]
     public function addToCart(
-        CartService $cartService,
-        int $id
-    ): Response {
+        CartService     $cartService,
+        Request         $request,
+        RouterInterface $router,
+        int             $id
+    ): ?Response
+    {
+        $routeReferer = (string)$request->headers->get('referer');
+        $refererPathInfo = Request::create($routeReferer)->getPathInfo();
+        $routeInfos = $router->match($refererPathInfo);
+
+        if(!$routeInfos) return null;
+
+        unset($routeControllerName, $routeInfos['_controller']);
+
         $cartService->addToCart($id);
 
-        return $this->redirectToRoute('app_cart_index');
+        return $this->redirectToRoute($routeInfos['_route'], $routeInfos);
     }
 
     #[Route('/cart/remove/{id}', 'app_cart_remove')]
     public function removeToCart(
         CartService $cartService,
-        int $id
-    ): Response {
+        int         $id
+    ): Response
+    {
         $cartService->removeToCart($id);
 
         return $this->redirectToRoute('app_cart_index');
