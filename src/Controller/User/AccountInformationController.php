73181c59050cfb1326776d3695fdd1e1e19dc007
Maxiloud <maxime@les-tilleuls.coop>
2023-09-27 14:19:35 +0200

    Retrieve Password

--- src/Controller/User/AccountInformationController.php
+++ src/Controller/User/AccountInformationController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller\User;
 
-use App\Entity\User;
+use App\Form\UserInformationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,13 +17,40 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[isGranted('IS_AUTHENTICATED_FULLY')]
 class AccountInformationController extends AbstractController
 {
+    public function __construct(private readonly Security $security)
+    {
+    }
+
     #[Route('', 'informations')]
     public function userInformation(): Response
     {
-        $user = $this->getUser();
+        $user = $this->security->getUser();
 
         return $this->render('user/_informations.html.twig', [
             'user' => $user
+        ]);
+    }
+
+    #[Route('/editer', 'informations_edit')]
+    public function edit(
+        Request       $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $user = $this->security->getUser();
+
+        $form = $this->createForm(UserInformationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Informations modifiées');
+        }
+
+        return $this->render('user/_information_edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/UserInformationFormType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserInformationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prenom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adress email'
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse'
+            ])
+            ->add('zipcode', TextType::class, [
+                'label' => 'Code postale'
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville'
+            ])
+            ->add('country', TextType::class, [
+                'label' => 'Pays'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider mes modifications',
+                'attr' => [
+                    'class' => 'btn-outline-primary'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
