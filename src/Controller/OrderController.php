33d73092778a682b6ce556f43c46107ce8982dcf
Maxiloud <maxime@les-tilleuls.coop>
2023-10-03 16:22:18 +0200

    wip

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Service\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,15 +12,23 @@ use Symfony\Component\Routing\Annotation\Route;
 #[isGranted('IS_AUTHENTICATED_FULLY')]
 class OrderController extends AbstractController
 {
-    public function __construct(private readonly CartService $cartService)
+    public function __construct(
+        private readonly CartService $cartService,
+        private readonly Security $security
+    )
     {
     }
 
     #[Route('/create', name: 'create')]
     public function create(): Response
     {
+        $user = $this->security->getUser();
+
         $cart = $this->cartService->getFullCart();
 
-        return $this->render('');
+        return $this->render('order/index.html.twig', [
+            'cart' => $this->cartService->getFullCart(),
+            'total' => $this->cartService->getTotal()
+        ]);
     }
 }
